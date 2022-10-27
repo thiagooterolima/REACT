e8ce2221bee04596033b2b63dbf9eaff65509b39
thiagoamim <thiagoamim11@gmail.com>
2022-10-26 21:51:37 -0300

    Meu primeiro commit

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 //import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Categoria from './src/telas/CatProduto';
+import Pagamento from './src/telas/TelaPagamento';
 
 export default function App() {
   return (
     
    <View>
+    {/*
     <Categoria/>
-    
+  */}
+  <Pagamento/>
+  
     </View>
   
   );
---
+++ src/telas/TelaPagamento.js
@@ -0,0 +1,26 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+//import Categoria from './src/telas/CatProduto';
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+
+const Pagamento = () => {
+    return (
+        <SafeAreaView>
+            
+            
+        <View><Text>Scaramouch</Text></View>
+
+        </SafeAreaView>
+    );
+}
+
+export default Pagamento;
+
+const estilos = StyleSheet.create ({
+    bar: {
+        height: 50,
+        position: 'absolute',
+        backgroundColor: 'blue',
+    }
+})
