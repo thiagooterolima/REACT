8e2906254644ef9717e40caeffd2af1626382948
thiagoamim <thiagoamim11@gmail.com>
2022-11-02 17:41:29 -0300

    fazendo a tela de pagamentos

--- src/telas/TelaPagamento.js
+++ src/telas/TelaPagamento.js
@@ -2,12 +2,22 @@ import { SafeAreaView, StyleSheet, Text, View,Button,Alert } from 'react-native'
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import Vendedor from '../../componets/vendedor';
+import Dados from './ListagemDados';
  
 
 export default function Pagamento ({navigation}) {
     return (
         <SafeAreaView> 
           <Vendedor/>
+
+          <View style={estilos.pagamento}>
+                <Text  style={estilos.pagamentoTexto}>Forma de Pagamento</Text>
+            </View>
+
+            <View style={estilos.total}>
+                <Text style={estilos.total1}>Total: </Text>
+                <Text>R$: 0,00</Text>
+            </View>
 
         <View style={estilos.botao}>
             <View style={estilos.direito}>
@@ -17,6 +27,15 @@ export default function Pagamento ({navigation}) {
                  onPress={() => navigation.navigate('Confirmar Dados')}
                  />
             </View>     
+
+            
+            <View style={estilos.esquerdo}>
+            <Button
+                color='red'
+                title ="VOLTAR"
+                onPress={() => navigation.navigate('Categoria Produto')}
+                />
+            </View>         
 
         
 </View>    
@@ -45,6 +64,27 @@ const estilos = StyleSheet.create ({
     
     esquerdo:{
         paddingRight:190,
+    },
+
+    pagamentoTexto: {
+        fontWeight: 'none',
+        fontSize: 23,
+    },
+
+    pagamento:{
+        padding: 20,
+    },
+
+    total:{
+        flexDirection: 'row',
+        paddingLeft: 25,
+        
+        
+    },
+
+    total1: {
+        color: 'green',
+        fontWeight: 'bold',
     }
-    
-})
+   
+});
